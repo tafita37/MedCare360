bd1c32ba31a8b5209194846d39038e9424647fb3
tafita37 <118960434+tafita37@users.noreply.github.com>
2025-07-12 13:18:46 +0300

    fix(request_count_nurse)

--- medical_consultation/models/hospital_nurse.py
+++ medical_consultation/models/hospital_nurse.py
@@ -2,24 +2,22 @@ from odoo import models, fields
 
 class HospitalNurse(models.Model):
     _inherit = 'hospital.nurse'
-    _order = 'request_count desc'
 
     request_count = fields.Integer(
-        string="Consultation Requests To Do",
-        compute='_compute_request_count',
-        store=True
+        string="Requests To Do",
+        compute='_compute_request_count'
     )
 
     def _compute_request_count(self):
-        ConsultationRequest = self.env['medical.consultation.request']
-        Consultation = self.env['medical.consultation.consultation']
+        consultationRequest = self.env['medical.consultation.request']
+        consultation = self.env['medical.consultation.consultation']
         for nurse in self:
             # Cherche les requests où nurse_id = nurse.id
-            requests = ConsultationRequest.search([('nurse_id', '=', nurse.id)])
+            requests = consultationRequest.search([('nurse_id', '=', nurse.id)])
             # Filtre celles qui n'ont pas encore de consultation liée
             count = 0
             for req in requests:
-                has_consult = Consultation.search_count([('request_id', '=', req.id)])
+                has_consult = consultation.search_count([('request_id', '=', req.id)])
                 if not has_consult:
                     count += 1
             nurse.request_count = count
